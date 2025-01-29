b9068e0c81e872222bae2aebfee6992c467b32f8
Zizii2 <166628072+Zizii2@users.noreply.github.com>
2025-01-29 14:15:51 +0100

    final

--- ovning/ovning_1/main/hello_world_main.c
+++ ovning/ovning_1/main/hello_world_main.c
@@ -14,12 +14,64 @@
 #include "esp_flash.h"
 #include "esp_system.h"
 
+#define ANSI_RED "\033[0;31m"
+#define ANSI_GREEN "\033[0;32m"
+#define ANSI_YELLOW "\033[0;33m"
+#define ANSI_BLUE "\033[0;34m"
+#define ANSI_MAGENTA "\033[0;35m"
+#define ANSI_CYAN "\033[0;36m"
+#define ANSI_RESET "\033[0m"
+#define NEW_LINE "\n"
+
+#define PRINTF_COLOR(color, format_string, ...) \
+    printf("%s" format_string "%s", color, ##__VA_ARGS__, ANSI_RESET NEW_LINE)
+
+#define PRINTF_GROUP_1(format, ... )\
+    PRINTF_COLOR(ANSI_BLUE, "[Group 1] "); \
+    PRINTF_COLOR(ANSI_RESET, format, ##__VA_ARGS__)
+
+/*
+    debug macros
+*/
+// __LINE__
+//__FILE__
+#define PRINTF_COLOR_WITH_LINE(color, format_string, ...) \
+    PRINTF_COLOR(ANSI_MAGENTA, "[%s:%d] ", __FILE__, __LINE__); \
+    PRINTF_COLOR(color, format_string, ##__VA_ARGS__)
+
+
 const static char *const TAG = "MAIN";
 
 void app_main(void)
 {
-    printf("Hello world!\n");
-    ESP_LOGI(TAG, "Hello world for my %d time", 1);
+    printf(ANSI_RED "Hello" ANSI_RESET " world!\n");
+    ESP_LOGI(TAG, "Hello world for my %d time\n", 1);
+    //ESP_LOGW (för varningar)
+    //ESP_LOGE (för errors)
+
+    const char *test_string = "test";
+    printf("%s%s%s",ANSI_RED, test_string, ANSI_RESET NEW_LINE);
+
+    PRINTF_COLOR(ANSI_BLUE, "%s" NEW_LINE, test_string);
+
+    PRINTF_GROUP_1("Hello world %d" NEW_LINE, 1);
+
+    PRINTF_COLOR_WITH_LINE(ANSI_YELLOW, "HEHEHEEHhehehe" NEW_LINE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /* Print chip information */
     esp_chip_info_t chip_info;
