792b4839f0751e4cab553f1dae4ee83f59faed73
Zizii2 <166628072+Zizii2@users.noreply.github.com>
2025-02-07 10:13:12 +0100

    Uart communication added

--- ovning/ovning_4/main/main.c
+++ ovning/ovning_4/main/main.c
@@ -7,63 +7,80 @@
 
 #include "esp_task_wdt.h"
 
-#define intrp_pin               GPIO_NUM_12
-#define btn_ipt                 GPIO_NUM_15
-
-bool btn_pres = false;
+#define intrp_pin               GPIO_NUM_15
+#define btn_ipt                 GPIO_NUM_16
 
 static void IRAM_ATTR gpio_isr_handler(void *arg){
-    //TODO write code to do something
-
-    btn_pres = true;
-
-    gpio_isr_handler_add(intrp_pin, gpio_isr_handler, NULL);
-    gpio_intr_enable(intrp_pin);
-    
+    gpio_isr_handler_remove(intrp_pin);
+    bool *button = (bool *) arg;
+    *button = true;
+    // gpio_isr_handler_add(intrp_pin, gpio_isr_handler, NULL);
+    // gpio_intr_enable(intrp_pin);
 }
 
 static void init(void){
     gpio_config_t pin_config = {
-        .mode = GPIO_MODE_INPUT_OUTPUT_OD,
-        .intr_type = GPIO_INTR_NEGEDGE,
-        .pin_bit_mask = intrp_pin,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .pull_up_en = GPIO_PULLUP_ENABLE
+        .mode = GPIO_MODE_INPUT,
+        .intr_type = GPIO_INTR_POSEDGE,
+        .pin_bit_mask = 1ull << intrp_pin,
+        .pull_down_en = GPIO_PULLDOWN_ENABLE,
+        .pull_up_en = GPIO_PULLUP_DISABLE
     };
     gpio_config(&pin_config);
-
-    pin_config.mode = GPIO_MODE_INPUT;
-    pin_config.intr_type = GPIO_INTR_DISABLE;
-    pin_config.pin_bit_mask = btn_ipt;
-    pin_config.pull_down_en = GPIO_PULLDOWN_ENABLE;
-    pin_config.pull_up_en = GPIO_PULLUP_DISABLE;
-
     gpio_install_isr_service(0);
-    gpio_isr_handler_add(intrp_pin, gpio_isr_handler, NULL);
-    gpio_intr_enable(intrp_pin);
+    // gpio_intr_enable(intrp_pin);
 
-    esp_task_wdt_config_t task_wdt = {
-        .timeout_ms = 3000,
-        .idle_core_mask = (1 << CONFIG_FREERTOS_NUMBER_OF_CORES) - 1,
-        .trigger_panic = true
-    };
-    ESP_ERROR_CHECK(esp_task_wdt_reconfigure(&task_wdt));
+    // pin_config.mode = GPIO_MODE_INPUT;
+    // pin_config.intr_type = GPIO_INTR_DISABLE;
+    // pin_config.pin_bit_mask = btn_ipt;
+    // pin_config.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    // pin_config.pull_up_en = GPIO_PULLUP_DISABLE;
+
+    // esp_task_wdt_config_t task_wdt = {
+    //     .timeout_ms = 3000,
+    //     .idle_core_mask = (1 << CONFIG_FREERTOS_NUMBER_OF_CORES) - 1,
+    //     .trigger_panic = false
+    // };
+    // ESP_ERROR_CHECK(esp_task_wdt_reconfigure(&task_wdt));
 }
 
+
+/*
+    !Keybindings ctrl + t + x == stoppa terminal
+*/
 void app_main(void)
 {
+    printf("HHHEEEEEJJ\n");
+    bool btn_pres = false;
+    bool interrupt_is_on = true;
     init();
-    ESP_ERROR_CHECK(esp_task_wdt_add(NULL));
+
+    gpio_isr_handler_add(intrp_pin, gpio_isr_handler, &btn_pres);
+
+    // ESP_ERROR_CHECK(esp_task_wdt_add(NULL));
+    int level = 0;
+    TickType_t button_pressed_time;
+    button_pressed_time = xTaskGetTickCount();
     while(true){
-        // int level = gpio_get_level(intrp_pin);
-        // printf("%d\n", level);
-        gpio_isr_handler_add(intrp_pin, gpio_isr_handler, NULL);
-        gpio_intr_enable(intrp_pin);
+        level = gpio_get_level(intrp_pin);
+        // printf("lvl: %d | bool: %d\n", level, &btn_pres);
         if(btn_pres){
-            btn_pres = false;
-            ESP_ERROR_CHECK(esp_task_wdt_reset());
+            // ESP_ERROR_CHECK(esp_task_wdt_reset());
             printf("RESET TASK\n");
-            vTaskDelay(pdMS_TO_TICKS(30));
+            btn_pres = false;
+            button_pressed_time = xTaskGetTickCount();
+            interrupt_is_on = false;
+            
         }
+
+        if(!interrupt_is_on){
+            TickType_t timeElapsed = xTaskGetTickCount() - button_pressed_time;
+            if (timeElapsed > pdMS_TO_TICKS(40)){
+                gpio_isr_handler_add(intrp_pin, gpio_isr_handler, &btn_pres);
+                interrupt_is_on = true;
+            }
+        }
+        // ESP_ERROR_CHECK(esp_task_wdt_reset());
+        vTaskDelay(pdMS_TO_TICKS(50));
     }
 }
