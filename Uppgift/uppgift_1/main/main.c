0ca435f98cd37eb851010fc84e03e9b7d5e3bc8b
Zizii2 <166628072+Zizii2@users.noreply.github.com>
2025-02-12 21:03:12 +0100

    binary led added, not tested

--- Uppgift/uppgift_1/main/main.c
+++ Uppgift/uppgift_1/main/main.c
@@ -2,8 +2,7 @@
 #include "Button.h"
 #include "Potentiometer.h"
 
-void app_main(void)
-{
+void app_main(void){
     button_handel btn1 = init_button(GPIO_INTR_DISABLE, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_DISABLE, GPIO_NUM_5);
     // adc1_channel_t channel = ADC1_CHANNEL_5;
     // Poten_handel poten = init_poten(&channel, NULL, ADC_ATTEN_DB_12, ADC_WIDTH_BIT_12);
@@ -13,13 +12,8 @@ void app_main(void)
         //     vTaskDelay(pdMS_TO_TICKS(30));
         // }
     while(1){
-        if ( update_button(btn1,xTaskGetTickCount()) ){
-            printf("Update true\n");
-        }
-        if (isPressed_button(btn1)){
-            printf("Button pressed\n");
-        }
-        // printf("...\n");
+        update_button(btn1, xTaskGetTickCount()); //! Needs to be check
+        if (isPressed_button(btn1)){ printf("Button pressed\n"); }
         vTaskDelay(pdMS_TO_TICKS(30));
     }
     free(btn1);
