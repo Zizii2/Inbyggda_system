6730491b057b962332a11f5c412f06b8d40f0040
Zizii2 <166628072+Zizii2@users.noreply.github.com>
2025-02-19 15:19:13 +0100

    sleep added

--- ovning/Ovning_6/main/main.c
+++ ovning/Ovning_6/main/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_sleep.h"
 #include "esp_err.h"
 
-#define one_sec (uint64_t)1000
+#define one_sec (uint64_t)1000000
 
 void app_main(void)
 {
-    int count = 0;
+    unsigned int count = 0;
     ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(one_sec));
     while(1){
-        ESP_ERROR_CHECK(esp_light_sleep_start());
+        esp_light_sleep_start();
         printf("%d\n", ++count);
     }
 }
