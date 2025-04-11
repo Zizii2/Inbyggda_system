cb94c233d5179adc883d654d656513e6ab6ab287
Zizii2 <166628072+Zizii2@users.noreply.github.com>
2025-04-11 15:05:34 +0200

    init of oled fixed

--- Project_locker/components/Screen/Screen.c
+++ Project_locker/components/Screen/Screen.c
@@ -1,15 +1,7 @@
 #include "Screen.h"
 
-// if i2c_master_bus_handle_t is NULL create one, maybe have I2C struct which keeps track of master_bus_handle
-// and the device handles
-i2c_master_dev_handle_t oled_lcd_init(void){
-    // ! REMOVE LATER
-    // i2c_clock_source_t clk_src_t;
-
-    i2c_master_bus_handle_t master_handle;
-    // This just make sure the isn't already any master_bus_handles
-    esp_err_t err = i2c_master_get_bus_handle(0, &master_handle);
-    if(err != ESP_OK){
+i2c_master_dev_handle_t oled_lcd_init(i2c_master_bus_handle_t master_handle){
+    if(master_handle == NULL){
         i2c_master_bus_config_t master_config = {
             .i2c_port = 0, // Use `i2c_master_get_bus_handle()` when you need the master_bus_handle in another source file
             .scl_io_num = GPIO_NUM_12,
@@ -22,6 +14,9 @@ i2c_master_dev_handle_t oled_lcd_init(void){
         };
         i2c_new_master_bus(&master_config, &master_handle);
     }
+    else{
+        ESP_ERROR_CHECK(i2c_master_get_bus_handle(0, &master_handle));
+    }
 
     i2c_device_config_t oled_dev_config = {
         .dev_addr_length = I2C_ADDR_BIT_LEN_7,
@@ -31,7 +26,7 @@ i2c_master_dev_handle_t oled_lcd_init(void){
     };
     i2c_master_dev_handle_t oled_dev_handle;
     i2c_master_bus_add_device(master_handle, &oled_dev_config, &oled_dev_handle);
-
+    ESP_LOGI("INIT", "NO problems with the init");
     return oled_dev_handle;
     /*
         ! Antekningar för mig
--- Project_locker/main/main.c
+++ Project_locker/main/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include <driver/gpio.h>
@@ -7,7 +8,13 @@
 #include "Screen.h"
 
 void app_main(void){
-    i2c_master_dev_handle_t oled_dev_handle = oled_lcd_init();
+    i2c_master_dev_handle_t oled_dev_handle = oled_lcd_init(NULL);
+    i2c_master_bus_handle_t master_handle;
+    ESP_ERROR_CHECK(i2c_master_get_bus_handle(0, &master_handle));
+    if(oled_dev_handle == NULL){
+        ESP_LOGE("DEVICE OLED LCD", "handle not found!");
+        return;
+    }
     oled_lcd_print(oled_dev_handle);
     return;
 }
