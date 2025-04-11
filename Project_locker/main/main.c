670f91c27086f379ce890fb33b9f1985a1a59270
Zizii2 <166628072+Zizii2@users.noreply.github.com>
2025-04-11 15:05:59 +0200

    removed some debug code in main

--- Project_locker/main/main.c
+++ Project_locker/main/main.c
@@ -9,12 +9,6 @@
 
 void app_main(void){
     i2c_master_dev_handle_t oled_dev_handle = oled_lcd_init(NULL);
-    i2c_master_bus_handle_t master_handle;
-    ESP_ERROR_CHECK(i2c_master_get_bus_handle(0, &master_handle));
-    if(oled_dev_handle == NULL){
-        ESP_LOGE("DEVICE OLED LCD", "handle not found!");
-        return;
-    }
     oled_lcd_print(oled_dev_handle);
     return;
 }
